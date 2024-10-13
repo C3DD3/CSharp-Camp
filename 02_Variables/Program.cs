229d68e3a9544bde98ea3721748f60af4b5cc442
Eyüp Sinan Durdu <eyupsinan00@gmail.com>
2024-10-13 13:42:19 +0300

    first commit

---
+++ 02_Variables/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Double Değişkenler
+
+            //double number;
+
+            //number = 4.85;
+            //Console.WriteLine(number);
+            //Console.Read();
+
+            //Console.WriteLine("------ Fiyat Listesi ------");
+            //Console.WriteLine();
+
+            //double applePrices, orangePrices, strawberryPrices, potatoPrices, tomatoPrices;
+
+            //applePrices = 14.85;
+            //orangePrices = 20.95;
+            //strawberryPrices = 45;
+            //potatoPrices = 9.74;
+            //tomatoPrices = 6.88;
+
+            //Console.WriteLine("----- Elma Birim Fiyatı: " + applePrices + " TL");
+            //Console.WriteLine("----- Portakal Birim Fiyatı: " + orangePrices + " TL");
+            //Console.WriteLine("----- Çilek Birim Fiyatı: " + strawberryPrices + " TL");
+            //Console.WriteLine("----- Patates Birim Fiyatı: " + potatoPrices + " TL");
+            //Console.WriteLine("----- Domates Birim Fiyatı: " + tomatoPrices + " TL");
+
+            //double appleGram, orangeGram, strawberryGram, potatoGram, tomatoGram;
+
+            //appleGram = 1.245;
+            //orangeGram = 2.650;
+            //strawberryGram = 0.750;
+            //potatoGram = 4.859;
+            //tomatoGram = 3.745;
+
+            //double appleTotalPrices = appleGram * applePrices;
+            //double orangeTotalPrices = orangeGram * orangePrices;
+            //double strawberryTotalPrices = strawberryGram * strawberryPrices;
+            //double potatoTotalPrices = potatoGram * potatoPrices;
+            //double tomatoTotalPrices = tomatoGram * tomatoPrices;
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("----- Elmanın Toplam Fiyatı: " + appleTotalPrices);
+            //Console.WriteLine("----- Portakalın Toplam Fiyatı: " + orangeTotalPrices);
+            //Console.WriteLine("----- Çileğin Toplam Fiyatı: " + strawberryTotalPrices);
+            //Console.WriteLine("----- Patatesin Toplam Fiyatı: " + potatoTotalPrices);
+            //Console.WriteLine("----- Domatesin Toplam Fiyatı: " + tomatoTotalPrices);
+
+            //double shoppingTotalPrices = appleTotalPrices + orangeTotalPrices + strawberryTotalPrices + potatoTotalPrices + tomatoTotalPrices;
+
+            //Console.WriteLine("\n----- Toplam ücret = " + shoppingTotalPrices + " TL.");
+
+            #endregion
+
+            #region Char Değişkenler
+
+            //char symbol;
+            //symbol = 'a';
+
+            //Console.WriteLine(symbol);
+
+            #endregion
+
+            #region Klavyeden Veri Girişleri String Veriler
+
+            //String name, surname, age, ıdentificalID, city;
+
+            //Console.Write("Lütfen isminizi giriniz: ");
+            //name = Console.ReadLine();
+
+            //Console.Write("Lütfen soyisminizi giriniz: ");
+            //surname = Console.ReadLine();
+
+            //Console.Write("Lütfen yaşınızı giriniz: ");
+            //age = Console.ReadLine();
+
+            //Console.Write("Lütfen tc nizi giriniz: ");
+            //ıdentificalID = Console.ReadLine();
+
+            //Console.Write("Lütfen yaşadığınız şehri giriniz: ");
+            //city = Console.ReadLine();
+
+            //Console.Write("\n\nİsim: " + name);
+            //Console.Write("\nSoyisim: " + surname);
+            //Console.Write("\nYaş: " + age);
+            //Console.Write("\nTC: " + ıdentificalID);
+            //Console.Write("\nŞehir: " + city);
+            #endregion
+
+            #region Klavyeden Veri Girişleri Integer Veriler
+
+            //int sayi1;
+            //int sayı2;
+
+            //Console.WriteLine("Lütfen Çarpmak İstediğiniz İki Sayıyı Giriniz");
+            //Console.WriteLine();
+
+            //Console.Write("1. Sayı: ");
+            //sayi1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("2.Sayı: ");
+            //sayı2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("\n\nVerdiğiniz iki sayının çarpımı = " + sayi1 * sayı2 + "'dir. ");
+
+            #endregion
+
+            #region Klavyeden Veri Girişleri Integer Veriler
+
+            //double sayi1;
+            //double sayı2;
+
+            //Console.WriteLine("Lütfen Çarpmak İstediğiniz İki Sayıyı Giriniz");
+            //Console.WriteLine();
+
+            //Console.Write("1. Sayı: ");
+            //sayi1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("2.Sayı: ");
+            //sayı2 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("\n\nVerdiğiniz iki sayının çarpımı = " + sayi1 * sayı2 + "'dir. ");
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
